5c086cfc416043df696bce608bd09695a275ce00
Mingyong Cai <Mingyong Cai>
2020-01-22 18:39:04 -0500

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Wei Nie on 1/22/20.
+//  Copyright © 2020 Terrycai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
