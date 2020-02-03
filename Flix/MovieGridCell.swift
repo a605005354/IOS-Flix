bfe1af6998b55ca479572218eae42eb994ceaa66
Mingyong Cai <Mingyong Cai>
2020-02-02 21:48:48 -0500

    Add layout and movie details.

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Wei Nie on 2/2/20.
+//  Copyright © 2020 Terrycai. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
